7a093c80c81ae00144026c2fd91e3f4a2f24fc00
MikkelKJensen <panelfitness@gmail.com>
2016-06-14 20:28:19 +0200

    AminolatTest

---
+++ Aminolat101/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Pinterest
+//
+//  Created by Mikkel Jensen on 08/06/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController, UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate {
+  
+  
+  
+  var searchController : UISearchController!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.searchController = UISearchController(searchResultsController:  nil)
+    
+    self.searchController.searchResultsUpdater = self
+    self.searchController.delegate = self
+    self.searchController.searchBar.delegate = self
+    
+    self.searchController.hidesNavigationBarDuringPresentation = false
+    self.searchController.dimsBackgroundDuringPresentation = true
+    
+    self.navigationItem.titleView = searchController.searchBar
+    
+    self.definesPresentationContext = true
+  }
+  
+  func updateSearchResultsForSearchController(searchController: UISearchController) {
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func setStatusBarBackgroundColor(color: UIColor) {
+    
+    guard  let statusBar = UIApplication.sharedApplication().valueForKey("statusBarWindow")?.valueForKey("statusBar") as? UIView else {
+      return
+      
+    }
+    
+    statusBar.backgroundColor = UIColor.blueColor()
+  }
+}
+
+
+
+  //Changing Status Bar
+  func preferredStatusBarStyle() -> UIStatusBarStyle {
+    
+    //LightContent
+    return UIStatusBarStyle.LightContent
+    
+    //Default
+    //return UIStatusBarStyle.Default
+    
+  }
+  
